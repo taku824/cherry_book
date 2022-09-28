7aef26f942ec31bb92a9a1428f347a9a8a0c0a63
taku824 <augusttaku824@gmail.com>
2022-09-28 10:53:35 +0900

    synthプログラムの作成

--- proc/synth/effects.rb
+++ proc/synth/effects.rb
@@ -1,6 +1,22 @@
 module Effects
   def self.reverse
-    # ->(words) do
-    #   words.split(' ').map(&:reverse).join(' ')
+    ->(words) do
+      words.split(' ').map(&:reverse).join(' ')
+    end
   end
+
+  def self.echo(rate)
+    ->(words) do
+      words.each_char.map { |c| c == ' ' ? c : c * rate }.join
+    end
+  end
+
+  def self.loud(level)
+    ->(words) do
+      words.split(' ').push('!'*level).map(&:upcase).join(' ')
+      words.split(' ').map{ |word| word.upcase + '!' * level }.join(' ')
+    end
+  end
+
+
 end
--- proc/synth/word_synth.rb
+++ proc/synth/word_synth.rb
@@ -1,6 +1,3 @@
-#require effects.rb
-
-
 class WordSynth
   def initialize
     @effects = []
@@ -11,31 +8,10 @@ class WordSynth
   end
 
   def play(original_words)
-    # words = original_words
-    # @effects.each do |effect|
-    #   words = effect.call(words)
-    # end
-    # p words
+    words = original_words
+    @effects.each do |effect|
+      words = effect.call(words)
+    end
+    words
   end
-
 end
-
-
-
-
-# エフェクトなし
-synth = WordSynth.new
-synth.play('Ruby is fun!')
-
-# リバースエフェクト
-# synth = WordSynth.new
-# synth.add_effect(Effects.reverse)
-# synth.play('Ruby is fun!')
-
-# 全エフェクトを一度に適応
-# synth = WordSynth.new
-# synth.add_effect(Effects.echo(2))
-# synth.add_effect(Effects.loud(3))
-# synth.add_effect(Effects.reverse)
-# synth.play('Ruby is fun!')
-
